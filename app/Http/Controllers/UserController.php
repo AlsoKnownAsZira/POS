f8bc31b08a46171f16ad75f94cd32d624f7ee4ed
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-03-07 01:45:55 +0700

    feat: done prak 6 eloquent js 3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,13 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+//prak js 3
+// class UserController extends Controller
+// {
+//    public function index( $id, $name)
+//    {
+//        return view('UserView')
+//     ->with('id', $id)
+//     ->with('name', $name);
+//    }
+// }
 
-class UserController extends Controller
-{
-   public function index( $id, $name)
-   {
-       return view('UserView')
-    ->with('id', $id)
-    ->with('name', $name);
+//prak eloquent
+// class UserController extends Controller
+// {
+//    public function index()
+//    {
+//       //coba akses model UserModel
+//       $user = UserModel::all(); // ambil semua data dari tabel m_user
+//       return view('user', ['data' => $user]);
+//    }
+// }
+//prak eloquent 2
+// Class UserController extends Controller{
+//    public function index(){
+//       // tambah data user dengan eloquent model
+//       $data = [
+//          'username' => 'customer-1',
+//          'nama' => 'Pelanggan',
+//          'password' => Hash::make('12345'),
+//          'level_id' => 4
+//       ];
+//       UserModel::insert($data); //tambahkan data ke tabel m_user
+//       //coba akses model UserModel
+//       $user = UserModel::all(); // ambil semua data dari tabel m_user
+//       return view('user', ['data' => $user]);
+
+//    }
+// }
+// prak eloquent 3
+class UserController extends Controller{
+   public function index(){
+      // tambah/update data user edngan Eloquent Model
+      $data = [
+         'nama' => 'Pelanggan Pertama',
+      ];
+      UserModel::where('username','customer-1')->update($data); //update data ke tabel m_user
+      //coba akses model UserModel
+      $user = UserModel::all(); // ambil semua data dari tabel m_user
+      return view('user', ['data' => $user]);
    }
 }
---
+++ app/Models/UserModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_user'; //mendefnisikan nama tabel yang digunakan model
+    protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel
+
+    
+}
