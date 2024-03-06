5e263a78b5bba0c7f31bfdee930aeff7a6374f9f
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-03-07 04:14:06 +0700

    feat: done prak 2.2 js 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -69,14 +69,22 @@ use Illuminate\Support\Facades\Hash;
 // }
 
 //Prak JS Eloquent 2.1
-Class UserController extends Controller{
+// Class UserController extends Controller{
+//    public function index(){
+//       // $user = UserModel::find(1);
+//       // $user = UserModel::where('level_id',1)->first();
+//       // $user = UserModel::firstWhere('level_id',1);
+//       $user =  UserModel::findOr(1,['username','nama'], function(){
+//          abort(404);
+//       });
+//       return view('user', ['data' => $user]);
+//    }
+// }
+//Prak JS Eloquent 2.2
+class UserController extends Controller{
    public function index(){
-      // $user = UserModel::find(1);
-      // $user = UserModel::where('level_id',1)->first();
-      // $user = UserModel::firstWhere('level_id',1);
-      $user =  UserModel::findOr(20,['username','nama'], function(){
-         abort(404);
-      });
+      // $user = UserModel::findOrFail(1);
+      $user = UserModel::where('username','manager9')->firstOrFail();
       return view('user', ['data' => $user]);
    }
 }
