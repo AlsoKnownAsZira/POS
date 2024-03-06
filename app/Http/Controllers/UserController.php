642ba9b01ec0e6318ef1f5bd77be0a43c4888dca
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-03-07 03:51:03 +0700

    chore: revision and finalization prak 2.1 js 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -73,7 +73,10 @@ Class UserController extends Controller{
    public function index(){
       // $user = UserModel::find(1);
       // $user = UserModel::where('level_id',1)->first();
-      $user = UserModel::firstWhere('level_id',1);
+      // $user = UserModel::firstWhere('level_id',1);
+      $user =  UserModel::findOr(20,['username','nama'], function(){
+         abort(404);
+      });
       return view('user', ['data' => $user]);
    }
 }
