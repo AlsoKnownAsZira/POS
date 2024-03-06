d94b81638371a77859115147005cdfc47630d556
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-03-07 06:13:16 +0700

    feat: done prak 2.3 js 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,86 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
-//prak js 3
-// class UserController extends Controller
-// {
-//    public function index( $id, $name)
-//    {
-//        return view('UserView')
-//     ->with('id', $id)
-//     ->with('name', $name);
-//    }
-// }
 
-//prak eloquent
-// class UserController extends Controller
-// {
-//    public function index()
-//    {
-//       //coba akses model UserModel
-//       $user = UserModel::all(); // ambil semua data dari tabel m_user
-//       return view('user', ['data' => $user]);
-//    }
-// }
-//prak eloquent 2
-// Class UserController extends Controller{
-//    public function index(){
-//       // tambah data user dengan eloquent model
-//       $data = [
-//          'username' => 'customer-1',
-//          'nama' => 'Pelanggan',
-//          'password' => Hash::make('12345'),
-//          'level_id' => 4
-//       ];
-//       UserModel::insert($data); //tambahkan data ke tabel m_user
-//       //coba akses model UserModel
-//       $user = UserModel::all(); // ambil semua data dari tabel m_user
-//       return view('user', ['data' => $user]);
-
-//    }
-// }
-// prak eloquent 3
-// class UserController extends Controller{
-//    public function index(){
+class UserController extends Controller
+{
+   public function index()
+   {
+      $user = UserModel::where('level_id', 2)->count();
     
-//       // $data = [
-//       //    'nama' => 'Pelanggan Pertama',
-//       // ];
-//       // UserModel::where('username','customer-1')->update($data); //update data ke tabel m_user
-//       //prak 1 js eloquent 
-//       $data = [
-//             'level_id' =>2,
-//             // 'username'=>'manager_dua',
-//             // 'nama'=>'Manager 2',
-//             'username'=> 'manager_tiga',
-//             'nama'=> 'Manager 3',
-//             'password'=>Hash::make('12345')
-//          ];
-//          UserModel::create($data); //tambahkan data ke tabel m_user
-    
-//       //coba akses model UserModel
-//       $user = UserModel::all(); // ambil semua data dari tabel m_user
-//       return view('user', ['data' => $user]);
-//    }
-// }
-
-//Prak JS Eloquent 2.1
-// Class UserController extends Controller{
-//    public function index(){
-//       // $user = UserModel::find(1);
-//       // $user = UserModel::where('level_id',1)->first();
-//       // $user = UserModel::firstWhere('level_id',1);
-//       $user =  UserModel::findOr(1,['username','nama'], function(){
-//          abort(404);
-//       });
-//       return view('user', ['data' => $user]);
-//    }
-// }
-//Prak JS Eloquent 2.2
-class UserController extends Controller{
-   public function index(){
-      // $user = UserModel::findOrFail(1);
-      $user = UserModel::where('username','manager9')->firstOrFail();
       return view('user', ['data' => $user]);
    }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,21 +6,11 @@
     </head>
     <body>
         <h1>Data User</h1>
-        <table border="1" cellpadding="2" cellspacing="0">
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID level pengguna</th>
-            </tr>
-            {{-- @foreach ( $data as $d ) --}}
-                <tr>
-                    <td>{{$data->user_id}}</td>
-                    <td>{{$data->username}}</td>
-                    <td>{{$data->nama}}</td>
-                    <td>{{$data->level_id}}</td>
-                </tr>
-            {{-- @endforeach --}}
-        </table>
+<table border="3" cellpadding="10" cellspacing="5">
+    <tr>
+        <th>Total Pengguna</th>
+    </tr>
+    <tr><td>{{$data}}</td></tr>
+</table>
     </body>
 </html>
