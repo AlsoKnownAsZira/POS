27f529fd8c4d912d7c9c8f5cfa02b093f8c04a49
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-03-16 13:22:10 +0700

    feat: done tugas 1 js 5

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -23,8 +23,8 @@ class KategoriController extends Controller
     public function store(Request $request){
         
        KategoriModel::create([
-              'kategori_kode' => $request->kategori_kode,
-              'kategori_nama' => $request->kategori_nama,
+              'kategori_kode' => $request->kodeKategori,
+              'kategori_nama' => $request->namaKategori,
          ]);
          return redirect('/kategori');
     }
