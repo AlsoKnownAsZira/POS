f7fa5df3a84df499aad60afd0fe1ddaf75ca8487
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-03-25 00:55:52 +0700

    feat: done js 6 B

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -6,32 +6,43 @@ use Illuminate\Http\Request;
 // use Illuminate\Support\Facades\DB;
 use App\DataTables\KategoriDataTable;
 use App\Models\KategoriModel;
+use Illuminate\Http\RedirectResponse;
 
 class KategoriController extends Controller
 {
     public function index(KategoriDataTable $dataTable)
     {
-    
-   return $dataTable->render('kategori.index');
-   
+
+        return $dataTable->render('kategori.index');
     }
 
     public function create()
     {
         return view('kategori.create');
     }
-    public function store(Request $request){
-        
-       KategoriModel::create([
-              'kategori_kode' => $request->kodeKategori,
-              'kategori_nama' => $request->namaKategori,
-         ]);
-         return redirect('/kategori');
+    public function store(Request $request): RedirectResponse
+    {
+
+        //    KategoriModel::create([
+        //           'kategori_kode' => $request->kodeKategori,
+        //           'kategori_nama' => $request->namaKategori,
+        //      ]);
+
+        $validated = $request->validate([
+            'kategori_kode' => 'required',
+            'kategori_nama' => 'required',
+        ]);
+        KategoriModel::create([
+            'kategori_kode' => $validated['kategori_kode'],
+            'kategori_nama' => $validated['kategori_nama'],
+        ]);
+        return redirect('/kategori');
     }
-        /**
+    /**
      * Return to edit page
      */
-    function edit($id) {
+    function edit($id)
+    {
         return view('kategori.edit', ['data' => KategoriModel::find($id)]);
     }
 
@@ -47,10 +58,11 @@ class KategoriController extends Controller
         $kategori->save();
         return redirect('/kategori');
     }
-    function destroy($id) {
+
+    function destroy($id)
+    {
         KategoriModel::find($id)->delete();
-    
+
         return redirect('/kategori');
     }
-    
 }
--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -1,34 +1,51 @@
 @extends('layouts.app')
 
-{{-- Customize layout sections --}}
 @section('subtitle', 'Kategori')
 @section('content_header_title', 'Kategori')
 @section('content_header_subtitle', 'Create')
 
-{{-- Content body: main page content --}}
 @section('content')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <div class="container">
         <div class="card card-primary">
             <div class="card-header">
-                <h3 class="card-title">Buat Kategori baru</h3>
+                <h3 class="card-title">Buat Kategori Baru</h3>
             </div>
-
-            <form method="post" action="../kategori">
+            <form action="../kategori" method="post">
+                @csrf
                 <div class="card-body">
                     <div class="form-group">
                         <label for="kodeKategori">Kode Kategori</label>
-                        <input type="text" class="form-control" id="kodeKategori" name="kodeKategori" placeholder="">
+                        <input type="text" name="kategori_kode" id="kodeKategori" placeholder="Kode Kategori"
+                            class="form-control 
+                        @error('kategori_kode')
+                        is-invalid
+                        @enderror">
+
+                        @error('kategori_kode')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label for="namaKategori">Nama Kategori</label>
-                        <input type="text" class="form-control" id="namaKategori" name="namaKategori" placeholder="">
-                    </div>
-
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <input type="text" class="form-control" id="namaKategori" name="kategori_nama"
+                            placeholder="Nama Kategori">
                     </div>
                 </div>
+
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+
         </div>
-        </form>
     </div>
 @endsection
