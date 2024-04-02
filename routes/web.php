536a29bd433362a47c532d178c2adce24494df13
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-04-02 23:32:51 +0700

    feat: done prak 2 js 7

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\POSController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 //home
@@ -40,6 +41,7 @@ Route::post('/user/tambah_simpan',[UserController::class, 'tambah_simpan']);
 Route::get('/user/ubah/{id}',[UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}',[UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}',[UserController::class, 'hapus']);
+Route::get('/',[WelcomeController::class, 'index']);
 Route::resource('m_user',POSController::class);
 Auth::routes();
 
